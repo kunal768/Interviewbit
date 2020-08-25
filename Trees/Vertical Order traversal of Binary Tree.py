9d8e75adaae8aa37237db2d26c89ec03d19a55b6
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-08-25 19:36:04 +0530

    Update Vertical Order traversal of Binary Tree.py

--- Trees/Vertical Order traversal of Binary Tree.py
+++ Trees/Vertical Order traversal of Binary Tree.py
@@ -8,10 +8,10 @@ class Solution:
 	    hm = defaultdict(lambda : [])
 	    q = [(A,0)]
         for node in q :
-            hm[node[1]].append(node[0].val)
-            if node[0].left:
-                q.append((node[0].left,node[1]-1))
-            if node[0].right:
-                q.append((node[0].right,node[1]+1))
-	    return [hm[i] for i in sorted(hm)]
+        	hm[node[1]].append(node[0].val)
+	    	if node[0].left:
+				q.append((node[0].left,node[1]-1))
+	    	if node[0].right:
+				q.append((node[0].right,node[1]+1))
+		return [hm[i] for i in sorted(hm)]
 	    
