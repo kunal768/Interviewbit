28db3fdaa06a525f5798482155f66215320b3fab
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-08-25 19:34:13 +0530

    Create Vertical Order traversal of Binary Tree.py

---
+++ Trees/Vertical Order traversal of Binary Tree.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+	# @param A : root node of tree
+	# @return a list of list of integers
+	def verticalOrderTraversal(self, A):
+	    if not A :
+	        return []
+	    hm = defaultdict(lambda : [])
+	    q = [(A,0)]
+        for node in q :
+            hm[node[1]].append(node[0].val)
+            if node[0].left:
+                q.append((node[0].left,node[1]-1))
+            if node[0].right:
+                q.append((node[0].right,node[1]+1))
+	    return [hm[i] for i in sorted(hm)]
+	    
