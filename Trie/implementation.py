4d315b49257b115e25a24c1359c9962718dfea8b
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-08-27 17:35:02 +0530

    Create implementation.py

---
+++ Trie/implementation.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+class TrieNode :
+    def __init__(self):
+        self.children = defaultdict()
+        self.isEnd = False
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+        
+        
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        root = self.root
+        for w in word :
+            if not w in root.children :
+                root.children[w] = TrieNode()
+            root = root.children[w]
+        root.isEnd = True
+            
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        root = self.root
+        for w in word :
+            if w in root.children:
+                root = root.children[w]
+            else:
+                return 
+        # isEnd hona chahiye and null bhi nhi hona chahaie
+        return root and root.isEnd
+        
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        root = self.root
+        for w in prefix :
+            if w in root.children :
+                root = root.children[w]
+            else:
+                return False
+        
+        return True 
+        
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
