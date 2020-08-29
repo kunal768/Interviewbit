128639db04f6b4fdebbdbb848f5a6dfe040b8d00
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-08-29 18:46:57 +0530

    Update Kth smallest element BST.py

--- Trees/Kth smallest element BST.py
+++ Trees/Kth smallest element BST.py
@@ -16,4 +16,22 @@ class Solution:
         inorder(root,ans)
         return ans[k-1]
   
-  # Without Extra Space Using BST properties 
+# Without Storing BST nodes ( Iterative Inorder Traversal )
+class Solution:
+    # @param A : root node of tree
+    # @param B : integer
+    # @return an integer
+    def kthsmallest(self, root, k):
+        ans,stack = -1,[]
+        while stack or root :
+            while root :
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0 :
+                return root.val 
+            root = root.right
+        return ans
+            
+          
