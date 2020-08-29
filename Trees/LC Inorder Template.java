c5bf49a386c75c816b8cdcd3179db967bda8af98
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-08-29 18:57:45 +0530

    Create LC Inorder Template.java

---
+++ Trees/LC Inorder Template.java
@@ -0,0 +1,87 @@
+// LeetCode Validate BST.py
+/*
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        pre = None 
+        if not root :
+            return True 
+        stack = []
+        while stack or root  :
+            while root :
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if pre and pre.val >= root.val  :
+                return False
+            pre = root
+            root = root.right
+        return True
+            
+        ''' 
+ */
+ 
+// Question : Binary Tree Inorder Traversal
+
+public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<>();
+    if(root == null) return list;
+    Stack<TreeNode> stack = new Stack<>();
+    while(root != null || !stack.empty()){
+        while(root != null){
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        list.add(root.val);
+        root = root.right;
+        
+    }
+    return list;
+}
+
+//Question : Kth Smallest Element in a BST
+
+ public int kthSmallest(TreeNode root, int k) {
+     Stack<TreeNode> stack = new Stack<>();
+     while(root != null || !stack.isEmpty()) {
+         while(root != null) {
+             stack.push(root);    
+             root = root.left;   
+         } 
+         root = stack.pop();
+         if(--k == 0) break;
+         root = root.right;
+     }
+     return root.val;
+ }
+
+
+// Question : Validate Binary Search Tree
+public boolean isValidBST(TreeNode root) {
+   if (root == null) return true;
+   Stack<TreeNode> stack = new Stack<>();
+   TreeNode pre = null;
+   while (root != null || !stack.isEmpty()) {
+      while (root != null) {
+         stack.push(root);
+         root = root.left;
+      }
+      root = stack.pop();
+      if(pre != null && root.val <= pre.val) return false;
+      pre = root;
+      root = root.right;
+   }
+   return true;
+}
+
+
+
+ 
+ 
