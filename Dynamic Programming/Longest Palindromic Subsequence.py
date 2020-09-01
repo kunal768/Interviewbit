141e485138fedf59dfbca0c394429ad36072b02f
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-09-01 14:24:46 +0530

    Create Longest Palindromic Subsequence.py

---
+++ Dynamic Programming/Longest Palindromic Subsequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param A : string
+    # @return an integer
+    def solve(self, A):
+        def lcs(a,b):
+            n,m = len(a),len(b)
+            if n == 0 or m == 0 :
+                return 0 
+            dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+            for i in range(1,n+1):
+                for j in range(1,m+1):
+                    if a[i-1] == b[j-1]:
+                        dp[i][j] = 1 + dp[i-1][j-1]
+                    else:
+                        dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+            
+            return dp[n][m]
+        
+        return lcs(A,A[::-1])
+            
+            
