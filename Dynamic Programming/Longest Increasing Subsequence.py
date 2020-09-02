c09f2bbf38f06d3e62d85b91c865675c6a1749ee
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-09-02 20:30:43 +0530

    Create Longest Increasing Subsequence.py

---
+++ Dynamic Programming/Longest Increasing Subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def lis(self, A):
+        n = len(A)
+        dp = [1]*n
+        for i in range(1,n):
+            for j in range(i):
+                if A[i] > A[j] and dp[i] < dp[j]+1:
+                    dp[i] = dp[j]+1
+        return max(dp)
