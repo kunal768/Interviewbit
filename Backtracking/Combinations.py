23a7e9accdbf09f6ba6ed39e6310001b036d6307
kunal768 <33108756+kunal768@users.noreply.github.com>
2020-09-06 13:59:46 +0530

    Update Combinations.py

--- Backtracking/Combinations.py
+++ Backtracking/Combinations.py
@@ -7,11 +7,10 @@ class Solution:
         ans = []
         def dfs(path,index):
             nonlocal ans
-            if index == n :
-                if len(path) == k :
-                    if not path in ans :
-                        ans.append(path)
-                    return 
+            if len(path) == k :
+                if not path in ans :
+                    ans.append(path)
+                return 
             for i in range(index,n):
                 dfs(path+[A[i]],i+1)
                 dfs(path,i+1)
